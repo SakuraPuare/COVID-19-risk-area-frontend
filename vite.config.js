0c0242754c6f2fd6a2997b6afec281d052de2df9
Moder Steven <java20131114@gmail.com>
2022-09-09 13:48:27 +0800

    🔧 build: Vite Build Config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,27 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
-
 export default defineConfig({
   plugins: [
     vue(),
     Components({
-      resolvers: [
-        AntDesignVueResolver(),
-      ],
+      resolvers: [AntDesignVueResolver()],
     }),
   ],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
